8ffa6fb362d9bcd1d9f3bc24b9633c8868176013
Daniel Fernandes <daplf96@gmail.com>
2024-08-02 00:22:34 +0100

    Add lets go tracker integration and tweak some elements on all layouts

--- graphics/js/layouts/game-layout.js
+++ graphics/js/layouts/game-layout.js
@@ -6,6 +6,10 @@ $(() => {
 
 	loadFromSpeedControl();
 
+	if ($('.catch-count').length > 0) {
+		loadFromLetsGoTracker();
+	}
+
 	function loadFromSpeedControl() {
 		const speedcontrolBundle = 'nodecg-speedcontrol';
 
@@ -68,6 +72,69 @@ $(() => {
 				// The key here maps to the HTML element ID.
 				fadeText('#' + key, val, true);
 			});
+		}
+	}
+
+	function loadFromLetsGoTracker() {
+		setTimeout(updateCatchCounts, 500);
+		setInterval(updateCatchCounts, 5000);
+	}
+
+	function updateCatchCounts() {
+		// If we have data stored from Speedcontrol
+		if (runData) {
+			let trackerKeysMap = {};
+			const teams = runData.teams;
+
+			let i = 1;
+
+			// Collect the runner keys and their corresponding runner index.
+			for (let team of teams) {
+				for (let player of team.players) {
+					const trackerKey = player.customData.trackerKey;
+
+					if (trackerKey) {
+						trackerKeysMap[player.customData.trackerKey] = i;
+					}
+
+					i++;
+				}
+			}
+
+			const trackerKeys = Object.keys(trackerKeysMap);
+
+			if (trackerKeys && trackerKeys.length > 0) {
+				// Execute the request to fetch the tracker data.
+				const url = "https://us-central1-lets-go-tracker.cloudfunctions.net/app/get?names=" + Object.keys(trackerKeysMap).join(",");
+
+				fetch(url).then(function(response) {
+					return response.json();
+				}).then(function(trackerData) {
+					// For each runner in the tracker data response.
+					trackerData.forEach(runnerTrackerData => {
+						const trackerKey = runnerTrackerData.name;
+						const numberOfPokes = runnerTrackerData.pokes.filter(poke => poke.status === "marked").length;
+
+						// Get the runner ID from the tracker key. This will be used to update the HTML.
+						const runnerId = trackerKeysMap[trackerKey];
+
+						// If there is a runner ID.
+						if (runnerId) {
+							// Update the catch count on the layout.
+							$('#catch-count' + runnerId).text(numberOfPokes + "/135");
+
+							// If the runner is not done, we can show the catch count. Otherwise, it stays hidden so we can show the timer instead.
+							const finished = $('#finalTime' + runnerId).text().length > 0;
+							if (!finished) {
+								$('#finalTime' + runnerId).hide();
+								$('#catch-count' + runnerId).show();
+							}
+						}
+					});
+				}).catch(function(err) {
+					console.log('Fetch Error: ', err);
+				});
+			}
 		}
 	}
 });
